1f69f43c5f57cf2abec386c145865a622b8affe0
H. Wen <p283369@ai178213.ai.rug.nl>
2019-07-17 16:32:49 +0200

    Projection can be true or false when the dimension is different

--- ResNet/residual.py
+++ ResNet/residual.py
@@ -9,8 +9,8 @@ def residual_block(x, out_channels, projection = False, name = 'residual', block
 	# Get the input channels
 	input_channels = int(x.get_shape()[-1])
 
-	conv1 = conv_layer(x, 3, 3, out_channels, stride = 1, name = '{}_conv1'.format(name),activation_function=block_activation_function,is_batch_normalization=block_is_batch_normalization)
-	conv2 = conv_layer(conv1, 3, 3, out_channels, stride = 1, name = '{}_conv2'.format(name),activation_function=block_activation_function,is_batch_normalization=block_is_batch_normalization)
+	conv1 = conv_layer(x, 3, 3, out_channels, stride = 1, name = '{}_conv1'.format(name), activation_function=block_activation_function, is_batch_normalization=block_is_batch_normalization)
+	conv2 = conv_layer(conv1, 3, 3, out_channels, stride = 1, name = '{}_conv2'.format(name), activation_function=block_activation_function, is_batch_normalization=block_is_batch_normalization)
 
 	# What type of shortcut connection to use
 	if input_channels != out_channels:
@@ -20,10 +20,14 @@ def residual_block(x, out_channels, projection = False, name = 'residual', block
 			shortcut = conv_layer(x, 1, 1, out_channels, stride = 1, name = '{}_shortcut'.format(name),activation_function=block_activation_function,is_batch_normalization=block_is_batch_normalization)
 		else:
 			# Option A: Identity mapping with Zero-Padding
-			# This method doesn't introduce any extra parameters.
+			# Option A can also be called Identity mapping due to the original paper.
+			
+			# In original paper, the shortcut connections in Eqn.(1) introduce neither extra parameter nor computation complexity. How about if shortcut = tf.pad()
+			# It still does not introduce extra parameter and computation complexity???
+			# Answer: Option A also doesn't introduce any extra parameters.
 			shortcut = tf.pad(x, [[0, 0], [0, 0], [0, 0], [0, out_channels - input_channels]])
 	else:
-		# Identity mapping.
+		# Identity mapping.Hh
 		shortcut = x
 
 
