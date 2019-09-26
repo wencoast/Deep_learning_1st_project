5f392df89eee2519ba1ef872770a0165dc933a72
H. Wen <p283369@ai178213.ai.rug.nl>
2019-09-27 00:01:23 +0200

    sys.argv[1:] usage

--- ResNet/standard_for_outside_arguments.py
+++ ResNet/standard_for_outside_arguments.py
@@ -10,3 +10,25 @@ def parse_flags():
 if __name__ == '__main__':
 #    args = parse_args()
     arguments = parse_flags()
+
+"""
+
+One more style
+
+"""
+
+def parse_flags(argv):
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config_path', type=str, help='path to config file',
+                        default='./configs/config_ms1m_100.yaml')
+    return parser.parse_args(argv)
+# sys.argv[1:] is special.
+
+def main(args):
+    
+
+if __name__ == '__main__':
+    main(parse_flags(sys.argv[1:]))
+# Here there has no any problem, since args is same to parse_flags(sys.argv[1:])
+
